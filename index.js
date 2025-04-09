3bbdcfa8d609707de4ffea65f650901ade5d50ae
devjob4475 <thananchai216@gmail.com>
2025-04-10 01:36:48 +0700

    v.155sda

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 const express = require("express");
+const cors = require("cors"); // <-- เพิ่มตรงนี้
 const axios = require("axios");
 const app = express();
 const port = 8080;
 
+app.use(cors()); // <-- ให้ allow ทุก origin
 app.use(express.json());
 
 app.post("/save-m3u", async (req, res) => {
